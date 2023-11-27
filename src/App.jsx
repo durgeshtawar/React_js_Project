3da0b9ff7113133e0442b454b40cff73628c0bb3
DURGESH TAWAR <123550031+durgeshtawar@users.noreply.github.com>
2023-11-27 14:16:09 +0530

    BG_Changers
    This project works on changing the background of a web page .......;

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,40 @@
-import React from 'react'
-import Slideshow from './components/Slideshow';
+import { useState } from "react"
 
-const App = () => {
-  const images = [
-    {
-      url: "https://images.unsplash.com/photo-1551773188-0801da12ddae?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1565992441121-4367c2967103?q=80&w=2023&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1528310782510-2f00164b609f?q=80&w=2030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1518214598173-1666bc921d66?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1571748087168-6008155a9a23?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1571748087168-6008155a9a23?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-  ];
-  
+
+function App() {
+  const [color, setColor] = useState("black")
   return (
-    <div className='h-screen flex justify-center items-center'>
-   <Slideshow>
-    {images.map((Image, i) =>{
-      <img src={images.url} key={i} className='w-full object-cover'/>
-
-    })}
-   </Slideshow>
-
-    </div>
-    )
+    <>
+     <div className="w-full h-screen duration-200 "
+       style={{backgroundColor: color}}
+       >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button 
+            onClick={()=> setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor: "red"}}
+            >RED</button>
+            <button 
+            onClick={()=> setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor: "green"}}
+            >GREEN</button>
+            <button 
+            onClick={()=> setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor: "blue"}}
+            >BLUE</button>
+            <button 
+            onClick={()=> setColor("orange")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor: "orange"}}
+            >orange</button>
+          </div>
+        </div>
+     </div>
+    </>
+  )
 }
 
 export default App

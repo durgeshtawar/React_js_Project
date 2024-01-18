2f913db28d751fdc5d249b079580ef73e884a12f
DURGESH TAWAR <123550031+durgeshtawar@users.noreply.github.com>
2024-01-18 21:52:49 +0530

    Otp-login using react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,17 @@
-import { useState } from "react"
 
+import './App.css'
+import PhoneOtpForm from './componets/PhoneOtpForm';
 
 function App() {
-  const [color, setColor] = useState("black")
+  
+
   return (
-    <>
-     <div className="w-full h-screen duration-200 "
-       style={{backgroundColor: color}}
-       >
-        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-            <button 
-            onClick={()=> setColor("red")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{backgroundColor: "red"}}
-            >RED</button>
-            <button 
-            onClick={()=> setColor("green")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{backgroundColor: "green"}}
-            >GREEN</button>
-            <button 
-            onClick={()=> setColor("blue")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{backgroundColor: "blue"}}
-            >BLUE</button>
-            <button 
-            onClick={()=> setColor("orange")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{backgroundColor: "orange"}}
-            >orange</button>
-          </div>
-        </div>
-     </div>
-    </>
-  )
+    <div className='App'>
+    <h1> Login with mobile</h1>
+     <PhoneOtpForm/>
+
+    </div>
+  );
 }
 
 export default App

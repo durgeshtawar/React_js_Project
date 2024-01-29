ec13b39a11be6e6a297c1437a10769d0324c02f3
DURGESH TAWAR <123550031+durgeshtawar@users.noreply.github.com>
2024-01-29 11:58:10 +0530

    Multi-selection in input box
    using react js .

---
+++ src/componets/Pill.jsx
@@ -0,0 +1,10 @@
+const Pill = ({image, text, onClick}) => {
+    return (
+      <span className="user-pill" onClick={onClick}>
+        <img src={image} alt={text} />
+        <span>{text} &times;</span>
+      </span>
+    );
+  };
+  
+  export default Pill;
